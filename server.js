d2090b4931cf80b21bdd59d5d1430a5ca698432e
rubickking04 <rubickking04@gmail.com>
2022-05-20 15:55:00 +0800

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 const app = express()
-app.use('/', serveStatic(path_join(__dirname, '/dist')))
+app.use('/', serveStatic(path(__dirname, '/dist')))
 const port = process.env.PORT || 8080
 app.listen(port)
 console.log('Listening port: ' + port)
