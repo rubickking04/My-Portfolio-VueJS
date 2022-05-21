2dfa36d2b9308c1efa04bb5ae12b7425fe5c22c3
rubickking04 <rubickking04@gmail.com>
2022-05-21 22:19:10 +0800

    first commit

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -4,12 +4,12 @@
         <h6 class="text-center text-uppercase text-secondary fw-bold py-5" style="font-family: 'Montserrat', sans-serif">• Projects •</h6>
         <h1 class="text-center text-capitalize fw-bold py-5" style="font-family: 'Montserrat', sans-serif" data-aos="fade-up" data-aos-anchor-placement="top-center" data-aos-easing="linear" data-aos-duration="900">My previous works</h1>
           <div class="row py-5 no-gutters">
-            <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-12 mb-5">
               <v-hover v-slot="{ hover }">
                 <v-card  :elevation="hover ? 24 : 2" :class="{ 'on-hover': hover  }" max-width="550">
-                <v-img src="../assets/project2.png" height="200px"></v-img>
+                <v-img src="../assets/project2.png" height="230px"></v-img>
                 <v-card-title>My Web Portfolio</v-card-title>
-                <v-card-subtitle class="subtitle-1">Al-Fhaigar Usman</v-card-subtitle>
+                <v-card-subtitle class="subtitle-1 fw-bold">Al-Fhaigar Usman</v-card-subtitle>
                 <v-card-text><v-btn href="https://vuejs.org/" elevation="2" large outlined color="#1b5e20" ><i class="fa-brands font-weight-bold fs-4 fa-vuejs px-2" style="color:#1b5e20;"></i>Vue JS</v-btn></v-card-text>
                 <v-card-actions>
                   <v-btn href="https://alfhaigarusman04.herokuapp.com/" color="#1b5e20" text>Explore Now</v-btn>
@@ -27,12 +27,12 @@
               </v-card>
               </v-hover>
             </div>
-            <div class="col-lg-6 col-md-6 col-sm-12 col-12 mb-3">
+            <div class="col-lg-6 col-md-6 col-sm-12 col-12 mb-5">
               <v-hover v-slot="{ hover }">
                 <v-card  :elevation="hover ? 24 : 2" :class="{ 'on-hover': hover  }" max-width="550">
-                <v-img src="../assets/project1.png" height="200px"></v-img>
-                <v-card-title>College of Information and Computing Science</v-card-title>
-                <v-card-subtitle class="subtitle-1">Automated Class Grading System</v-card-subtitle>
+                <v-img src="../assets/project1.png" height="230px"></v-img>
+                <v-card-title>System Software</v-card-title>
+                <v-card-subtitle class="subtitle-1 fw-bold" style="font-family: 'Montserrat', sans-serif">CICS - Automated Class Grading System</v-card-subtitle>
                 <v-card-text><v-btn href="https://laravel.com/" elevation="2" large outlined color="red" ><i class="fa-brands font-weight-bold fs-4 fa-laravel px-2" style="color:red;"></i>laravel</v-btn></v-card-text>
                 <v-card-actions>
                   <v-btn href="https://zppsu-cics-wannabe.herokuapp.com/" color="red" text>Explore Now</v-btn>
