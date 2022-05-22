c30937a27e4ea42ef31226bc68fc53e14404e0d1
rubickking04 <rubickking04@gmail.com>
2022-05-22 13:07:35 +0800

    first commit

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -8,7 +8,6 @@ export default new Vuetify({
     theme: {
         themes: {
             light: {
-            primary: colors.red.darken1, // #E53935
             secondary: colors.red.lighten4, // #FFCDD2
             accent: colors.indigo.base, // #3F51B5
             },
